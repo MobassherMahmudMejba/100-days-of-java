f9f64f63f6f08aefd59ed3ec2ab5ab0e368f2701
Mobassher Mahmud Mejba <mobasshermahmudmejba@gmail.com>
2026-06-19 21:39:10 +0600

    TYpe casting

--- Java Basics/Variables.java
+++ Java Basics/Variables.java
@@ -1,44 +1,30 @@
-public class Variables {
-    public static void main(String[] args) {
-        // ডেটা টাইপ ডিক্লেয়ারেশন
-        int myNum = 5;
-        float f = 4.5f;
-        float myfloatNum = (float)5.456;
-        double mydoubleNum = 5.8484946848489498;
-        char character = 'M';
-        String text = "Hello Welcome to Day 1 of JAVA Learning!";
-        String T = "I have " + myNum + " Kids";
-
-        // প্রিন্ট করা
-        System.out.println("Integer: " + myNum);
-        System.out.println("Float: " + f);
-        System.out.println("Another Float: " + myfloatNum);
-        System.out.println("Double: " + mydoubleNum);
-        System.out.println("Character: " + character);
-        System.out.println("Text: " + text);
-        System.out.println("String combination: " + T);
-
-        // Boolean লজিক
-        boolean b = false;
-        b = true;
-        boolean toBe = false;
-        b = toBe || !toBe; // b এখন true হয়ে যাবে
-        
-        if (b) {
-            System.out.println("toBe is: " + toBe);
+#include<stdio.h>
+#include<string.h>
+struct Customer{
+    int id;
+    char name[50];
+    float loan;
+    float rate;
+    int time;
+};
+int main() {
+    int n;
+    struct Customer c; 
+    int max_id = 0;
+    int max_total = 0;
+    char max_name[50];
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %s %f %f %d", &c.id, c.name, &c.loan, &c.rate, &c.time);
+        int interest =(c.loan*c.rate*c.time)/100;
+        int total=c.loan+interest;
+        printf("%d | %-5s | %-5d | %d\n", c.id, c.name, interest, total);
+        if (total>max_total) {
+            max_total=total;      
+            max_id=c.id;          
+            strcpy(max_name,c.name); 
         }
-
-        // সঠিক লজিক: int দিয়ে if কন্ডিশন
-        int children = 0;
-        if (children == 0) {
-            System.out.println("No children yet.");
-        }
-
-        // গাণিতিক অপারেশন (সংশোধিত)
-        int a;
-        int x = 10;
-        int y = 5;
-        a = x + y; // boolean যোগ করা যায় না, তাই int যোগ করলাম
-        System.out.println("Addition result: " + a);
     }
+    printf("\nHighest Payable Customer: %s (%d)", max_name, max_id);
+    return 0;
 }
